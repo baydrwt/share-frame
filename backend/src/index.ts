8aafe234e075575d03d44b3467e703c52759c498
baydrwt <bayu0825.bd@gmail.com>
2025-01-22 20:31:46 +0700

    db configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 const app = express();
+import connectDb from "./config/db";
+import dotenv from "dotenv";
 
-app.listen(8000, () => {
-  console.log("server are running on port 8000");
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+connectDb();
+app.listen(port, () => {
+  console.log(`server are running on port ${port}`);
 });
