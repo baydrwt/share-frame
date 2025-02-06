11c445c152e3630fa97fb3d835037981882d7601
baydrwt <bayu0825.bd@gmail.com>
2025-02-06 15:02:30 +0700

    fix error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,6 +21,13 @@ app.options("*", cors(corsOptions));
 
 app.use(passportJwtStrategy.initialize());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://share-frame.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
