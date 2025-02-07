397046a2fdce5a08ef762c0cb88fa18dac727f9d
baydrwt <bayu0825.bd@gmail.com>
2025-02-07 15:30:42 +0700

    fixing issues

--- frontend/src/api/backendApi.ts
+++ frontend/src/api/backendApi.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
 
 const backendApi = axios.create({
   // baseURL: "http://localhost:8000",
   baseURL: "https://share-frame-backend-api.vercel.app",
-  withCredentials: true,
 });
 
 export default backendApi;
